cce21e5e7f60c1109fed067b90a97dc531a7ecfb
Mahmoud <mahmoud.s.badran@gmail.com>
2020-09-30 02:05:46 +0200

    add 1D convolution function with padding

--- AIAChallenge.py
+++ AIAChallenge.py
@@ -7,11 +7,54 @@ class ConvolutionOps:
     # Constructor 
     def __init__(self):
         pass
+    
+    def convolution1D(self, signal, kernel):
+        # initalised a padded signal depends on the filter(kernel) size
+        newPaddedSignal = np.zeros((signal.shape[0] + (kernel.shape[0]-1),))
+        print(newPaddedSignal.shape)
+        # Add the content of signal to the padded signal with
+        # keeping the padded element of zero value.
+        # kernel.shape[1]-1) this to know how many elements to add  
+        # at the end and at the start of the signal
+        for i in range(newPaddedSignal.shape[0]):
+            if i < ((kernel.shape[0]-1) /2):
+                newPaddedSignal[i] = 0
+            elif i > ((newPaddedSignal.shape[0]-1) - (kernel.shape[0]-1) /2):
+                newPaddedSignal[i] = 0
+            else :
+                index = int(i- ((kernel.shape[0]-1) /2))
+                newPaddedSignal[i] = signal[index]
+
+      
+            
+        #....................... Flip the 1D kernel ..............................
+        
+        # initalize a flipped kernel 
+        flipped1DKernel = np.zeros((kernel.shape[0],))
+        kernelLastIndex = kernel.shape[0]-1 
+        for i in range (kernel.shape[0]):
+            # swap operation(sorting) 
+            flipped1DKernel[i] = kernel[kernelLastIndex - i]
+        print(flipped1DKernel , newPaddedSignal)
+        #................ start the convolution process ............................ 
+        # the newly convolved signal size will be the same as the original signal 
+        # because of the padding.
+        # initalize a covolved signal 
+        newConvoledSignal = np.zeros((signal.shape[0], ))
+        sumRes = 0
+        for i in range(newConvoledSignal.shape[0]):
+            for j in range(flipped1DKernel.shape[0]):
+                sumRes = sumRes + (flipped1DKernel[j] * newPaddedSignal[i+j])
+            newConvoledSignal[i] = sumRes
+            sumRes = 0
+        return newConvoledSignal   
         
         
-    # Convolution Function     
+    # ............................2D Convolution Function, no padding      
     def convolve2D(self, signal, kernel):
         # Find number of shifting (Row and Column), no padding
+        # shift horizentally: column filter - column signal
+        # shift vertically: row filter - row signal 
         numOfRowToShift = np.abs(signal.shape[0]-kernel.shape[0]) + 1 
         numOfColumnToShift = np.abs(signal.shape[1]-kernel.shape[1]) + 1 
         flippedKernel = self.flipFilter(kernel)
@@ -32,8 +75,8 @@ class ConvolutionOps:
     #Flipping the Kernel 180 degree for convolution     
     def flipFilter(self, kernelTest): 
         
-        firstFlippedKernel = np.zeros((kernelTest.shape[0],kernelTest.shape[1])) # temp variable 
-        finalFlippedKernel = np.zeros((kernelTest.shape[0],kernelTest.shape[1])) # flipped kernel
+        firstFlippedKernel = np.zeros((kernelTest.shape[0],kernelTest.shape[1])) # initialize fipped kernel, first flip 
+        finalFlippedKernel = np.zeros((kernelTest.shape[0],kernelTest.shape[1])) # initalize flipped kernel, second flip 
         
         # First flipping operation, inverse clock wise 90 degree
         lastIndexRow = kernelTest.shape[0]-1
@@ -52,20 +95,31 @@ class ConvolutionOps:
         return finalFlippedKernel 
     
     
-    
+ #................................... Testing............................... 
+# Create Object Of Convolution class   
 startConv = ConvolutionOps()
 
+# ...................Testing Convolution 2D function .....................
 signal = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
 kernel = np.array([[1,0,1],
                   [0,1,1],
                   [0,1,1]])
 kernel2 = np.array([[0,0],
                     [0,0]])
+# First Flip the kernel , call flip function
+flippedKernel = startConv.flipFilter(kernel)  
+# CAll the 2d Conv function 
+print('Convolved Signal 2D ', startConv.convolve2D(signal,flippedKernel) )
 
-#flippedKernel = startConv.flipFilter(kernel)   
-#print(kernel)  
-#print(flippedKernel)  
-print(startConv.convolve2D(signal, kernel2))
-    
+#............. Testing Convolution 1D ......................
+signal2 = np.array([1,2,3,2,2,1,1])
+kernel3 = np.array([1,1,0,1,0])
+
+signal3 = np.array([1,2,3,4])
+kernel4 = np.array([0,1,0])
+# call the conv 1D fucntion, kernel should be of odd size 
+print('Convolved Signal 1D', startConv.convolution1D(signal3,kernel4))
+#print(startConv.convolve2D(signal, kernel2))
+
     
         
