f809430c41a4f02002f096d7f56d81ab50bf7b89
Mahmoud <mahmoud.s.badran@gmail.com>
2020-10-05 02:16:44 +0200

    add symmetry padding

--- AIAChallenge.py
+++ AIAChallenge.py
@@ -8,24 +8,48 @@ class ConvolutionOps:
     def __init__(self):
         pass
     
-    def convolution1D(self, signal, kernel):
-        # initalised a padded signal depends on the filter(kernel) size
-        newPaddedSignal = np.zeros((signal.shape[0] + (kernel.shape[0]-1),))
-        print(newPaddedSignal.shape)
-        # Add the content of signal to the padded signal with
-        # keeping the padded element of zero value.
-        # kernel.shape[1]-1) this to know how many elements to add  
-        # at the end and at the start of the signal
-        for i in range(newPaddedSignal.shape[0]):
-            if i < ((kernel.shape[0]-1) /2):
-                newPaddedSignal[i] = 0
-            elif i > ((newPaddedSignal.shape[0]-1) - (kernel.shape[0]-1) /2):
-                newPaddedSignal[i] = 0
-            else :
-                index = int(i- ((kernel.shape[0]-1) /2))
-                newPaddedSignal[i] = signal[index]
-
-      
+    def convolution1D(self, signal, kernel, paddingType="zeroPadding"):
+        # Condition for zero padding 
+        if paddingType == "zeroPadding":
+            # initalised a padded signal depends on the filter(kernel) size
+            newPaddedSignal = np.zeros((signal.shape[0] + (kernel.shape[0]-1),))
+            # Add the content of signal to the padded signal with
+            # keeping the padded element of zero value.
+            # kernel.shape[1]-1) this to know how many elements to add  
+            # at the end and at the start of the signal
+            for i in range(newPaddedSignal.shape[0]):
+                if i < ((kernel.shape[0]-1) /2):
+                    newPaddedSignal[i] = 0
+                elif i > ((newPaddedSignal.shape[0]-1) - (kernel.shape[0]-1) /2):
+                    newPaddedSignal[i] = 0
+                else :
+                    index = int(i- ((kernel.shape[0]-1) /2))
+                    newPaddedSignal[i] = signal[index]
+            print("zero padded signal:", newPaddedSignal)            
+        # Condition for symmetric padding 
+        if paddingType == "Symmetric":
+            # initalised a symmetry padded signal depends on the filter(kernel) size
+            newPaddedSignal = np.zeros((signal.shape[0] + (kernel.shape[0]-1),))
+            # use lastIndexSignal to know the last index of my signal to fill
+            # the symmetric values in the padded indecies 
+            lastIndexSignalFirstCond = signal.shape[0]-1 
+            lastIndexSignalSecCond = signal.shape[0]-1 
+            # Add the content of signal to the padded signal with
+            # keeping the padded element of zero value.
+            # kernel.shape[1]-1) this to know how many elements to add  
+            # at the end and at the start of the signal
+            for i in range(newPaddedSignal.shape[0]):
+                s = int((((kernel.shape[0]-1) /2)-1))
+                if i < ((kernel.shape[0]-1) /2):
+                    newPaddedSignal[s-i] = signal[i] 
+                    lastIndexSignalFirstCond = lastIndexSignalFirstCond -1
+                elif i > ((newPaddedSignal.shape[0]-1) - (kernel.shape[0]-1) /2):
+                    newPaddedSignal[i] = signal[lastIndexSignalSecCond]
+                    lastIndexSignalSecCond = lastIndexSignalSecCond -1
+                else :
+                    index = int(i- ((kernel.shape[0]-1) /2))
+                    newPaddedSignal[i] = signal[index]
+            print("padded symmety", newPaddedSignal)
             
         #....................... Flip the 1D kernel ..............................
         
@@ -35,7 +59,6 @@ class ConvolutionOps:
         for i in range (kernel.shape[0]):
             # swap operation(sorting) 
             flipped1DKernel[i] = kernel[kernelLastIndex - i]
-        print(flipped1DKernel , newPaddedSignal)
         #................ start the convolution process ............................ 
         # the newly convolved signal size will be the same as the original signal 
         # because of the padding.
@@ -100,26 +123,36 @@ class ConvolutionOps:
 startConv = ConvolutionOps()
 
 # ...................Testing Convolution 2D function .....................
+print('......Test 2D Convolution...........')
 signal = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
 kernel = np.array([[1,0,1],
                   [0,1,1],
                   [0,1,1]])
-kernel2 = np.array([[0,0],
-                    [0,0]])
+print('Signal:\n',signal)
+print('Filter:\n', kernel)
 # First Flip the kernel , call flip function
 flippedKernel = startConv.flipFilter(kernel)  
 # CAll the 2d Conv function 
-print('Convolved Signal 2D ', startConv.convolve2D(signal,flippedKernel) )
-
+print('Convolved Signal 2D \n', startConv.convolve2D(signal,flippedKernel) )
+print('........Test 1D convolution..............')
 #............. Testing Convolution 1D ......................
-signal2 = np.array([1,2,3,2,2,1,1])
-kernel3 = np.array([1,1,0,1,0])
 
 signal3 = np.array([1,2,3,4])
-kernel4 = np.array([0,1,0])
-# call the conv 1D fucntion, kernel should be of odd size 
-print('Convolved Signal 1D', startConv.convolution1D(signal3,kernel4))
-#print(startConv.convolve2D(signal, kernel2))
+kernel4 = np.array([0,1,1])
+
+# Print the signal,Filter we are testing on, change in the values
+# to check my function compared with numpy function 
+print('Signal:',signal3)
+print('Filter:', kernel4)
+print('Results.........................')
+# call the conv 1D fucntion, kernel should be of odd size, sym padding  
+print('Convolved Signal 1D with sym padding, my function:', startConv.convolution1D(signal3,kernel4,"Symmetric"))
+
+# call the conv 1D fucntion, kernel should be of odd size, 0 padding  
+print('Convolved Signal 1D with zero padding, my function:', startConv.convolution1D(signal3,kernel4,"zeroPadding"))
+
+
+print('convolved Signal using numpy function',np.convolve(signal3,kernel4))
 
     
         
